57fea39dd416601960f5e4bb1ff9e6029b41e5eb
Chris Monit <4059789+chrismonit@users.noreply.github.com>
2023-11-06 14:58:57 +0000

    added examples to path utilities

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ RAW_DIR = "raw"
 
 
 def get_root_path() -> str:
-    """ Automatically detect the full path leading to the repo folder.
+    """Automatically detect the full path leading to the repo folder.
     Assuming directory structure is:
         /this/project/path/repo_name/package/this_module.py
     Return:
@@ -22,16 +22,16 @@ def get_root_path() -> str:
 
 
 def get_project_path(*lower_paths):
-    """If repo directory is 
-            `/path/to/repo/project`
-        and `lower_paths` is `file.txt`
-        then return
-            `/path/to/repo/project/file.txt`
+    """If repo directory is
+        `/path/to/repo/project`
+    and `lower_paths` is `file.txt`
+    then return
+        `/path/to/repo/project/file.txt`
 
-        Alternatively, if `lower_paths` is ["subfolder", "file.txt"]
-        then return
-            `/path/to/repo/project/subfolder/file.txt`
-        etc for any number of subfolder layers.
+    Alternatively, if `lower_paths` is ["subfolder", "file.txt"]
+    then return
+        `/path/to/repo/project/subfolder/file.txt`
+    etc for any number of subfolder layers.
     """
     root = get_root_path()
     path = os.path.join(root, *lower_paths)
@@ -55,8 +55,25 @@ def get_processed_path(*lower_paths):
 
 def get_output_path(*lower_paths):
     path = get_project_path(DATA_DIR, OUTPUT_DIR, *lower_paths)
-    return path 
+    return path
 
 
 if __name__ == "__main__":
+    # Examples assuming path to project is: `/path/to/repo/tim-tam-repo`
+    print(get_root_path())
+    # `/path/to/repo/tim-tam-repo`
+
+    print(get_static_path("test.txt"))
+    # `/path/to/repo/tim-tam-repo/static/test.txt`
+
+    print(get_raw_path("test.txt"))
+    # `/path/to/repo/tim-tam-repo/raw/test.txt`
+
+    print(get_processed_path("test.txt"))
+    # `/path/to/repo/tim-tam-repo/processed/test.txt`
+
     print(get_output_path("test.txt"))
+    # `/path/to/repo/tim-tam-repo/output/test.txt`
+
+    print(get_output_path("folder1", "folder2", "test.txt"))
+    # `/path/to/repo/tim-tam-repo/output/folder1/folder2/test.txt`
